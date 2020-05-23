fb32ee6c1caf23c61baf8afad9fa771303306526
Jason Vernot <jason.vernot@gmail.com>
2020-05-23 12:27:37 -0400

    working on api calls

--- public/js/games.js
+++ public/js/games.js
@@ -1,4 +1,62 @@
 
+let platformId = $('#platformDrop').val();
+
+switch (platformId) {
+    case "PC":
+        platformId = 4;
+        break;
+
+    case "Playstation 5":
+        platformId = 187;
+        break;
+
+    case "Xbox One":
+        platformId = 1;
+        break;
+
+    case "Playstation 4":
+        platformId = 18;
+        break;
+
+    case "Xbox Series X":
+        platformId = 186;
+        break;
+
+    case "Nintendo Switch":
+        platformId = 7;
+        break;
+
+    case "Xbox 360":
+        platformId = 14;
+        break;
+
+    case "Playstation 3":
+        platformId = 16;
+        break;
+
+    case "Playstation 2":
+        platformId = 15;
+        break;
+
+    case "Wii":
+        platformId = 11;
+        break;
+
+    case "GameCube":
+        platformId = 105;
+        break;
+
+    case "SNES":
+        platformId = 79;
+        break;
+};
+
+let genre = $('#genreDrop').val();
+
+
+
+
+
 const gameSearch = () => {
     const searchQuery = $('#searchBar').val();
 
