bafae00197bae44339fbe1559958a8fbc72846df
Samuel Guevara <samsjamin@outlook.com>
2020-05-23 11:24:08 -0400

    updated html

---
+++ public/js/homepage.js
@@ -0,0 +1,11 @@
+$(".default_option").click(function(){
+    $(".dropdown ul").addClass("active");
+});
+  
+  $(".dropdown ul li").click(function(){
+    var text = $(this).text();
+    $(".default_option").text(text);
+    $(".dropdown ul").removeClass("active");
+ });
+
+ 
