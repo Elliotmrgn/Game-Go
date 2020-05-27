a57cb52179a9cd8a98fb4b57f9d034479a3a714d
Elliot <elliotmrgn@gmail.com>
2020-05-27 16:51:54 -0400

    sample output working for homepage

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -23,11 +23,12 @@ $(document).ready(function () {
             const randGame = Math.ceil(Math.random() * result.count)
             console.log("randGame", randGame)
             //finds what page a game is on
-            const gamePage = Math.ceil(randGame/20);
+            const gamePage = Math.ceil(randGame / 20);
             console.log("gamePage", gamePage)
-            //find what array slot is in
-            let gameSlot = (randGame%20)-1;
-            if (gameSlot === -1) {gameSlot=19;}
+            //find what array slot its in
+            let gameSlot = (randGame % 20) - 1;
+            //case for last slot ****should be a better solution than this
+            if (gameSlot === -1) { gameSlot = 19; }
             console.log("gameSlot", gameSlot)
             //new ajax call for that page
             queryUrl += `&page=${gamePage}`;
@@ -37,8 +38,20 @@ $(document).ready(function () {
             }).then(results => {
                 //new page results 
                 console.log("RANDOM PAGE RESULTS", results)
-                //picks a random game
-                console.log(results.results[gameSlot])
+                //destructuring results to game
+                const { game } = { game: results.results[gameSlot] }
+                console.log(game)
+                //title update
+                $(".title").text(game.name)
+                //screenshot update
+                $(".screenshot").attr("src", game.background_image)
+                //creates an array of all platforms for the game and updates
+                const allPlatforms = []
+                game.platforms.forEach(platform => {
+                    allPlatforms.push(` ${platform.platform.name}`)
+                });
+                $(".platforms").text(allPlatforms)
+                
             })
         })
     });
