9efcd100c07dfe09fc69fccd0a0c1665c07670ec
Elliot <elliotmrgn@gmail.com>
2020-05-27 16:23:06 -0400

    random game working

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,24 +1,45 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('#search').on('click', (event) => {
         event.preventDefault();
+        //gets platform value
         let platform = $("input[name='platform']:checked").val();
-        console.log("platform----", platform)
+        //gets genre value
         let genre = $("input[name='genre']:checked").val();
-        console.log("genre----", genre)
-        let tags=$("input[name='tags']:checked").map(function(){
+        //gets all tag values
+        let tags = $("input[name='tags']:checked").map(function () {
             return $(this).val();
-          }).get();
-        console.log("tags----", tags)
+        }).get();
+        //combines for search query
         let searchQuery = `platforms=${platform}&genres=${genre}&tags=${tags}`;
-        console.log("searchQuery---", searchQuery);
 
-        const queryUrl = `https://api.rawg.io/api/games?${searchQuery}`;
-
+        let queryUrl = `https://api.rawg.io/api/games?${searchQuery}`;
+        //first ajax call for all results
         $.ajax({
-            url:queryUrl,
-            type:"GET"
-        }).then(result =>{
+            url: queryUrl,
+            type: "GET"
+        }).then(result => {
             console.log(result)
+            //picks a random game 1 - max
+            const randGame = Math.ceil(Math.random() * result.count)
+            console.log("randGame", randGame)
+            //finds what page a game is on
+            const gamePage = Math.ceil(randGame/20);
+            console.log("gamePage", gamePage)
+            //find what array slot is in
+            let gameSlot = (randGame%20)-1;
+            if (gameSlot === -1) {gameSlot=19;}
+            console.log("gameSlot", gameSlot)
+            //new ajax call for that page
+            queryUrl += `&page=${gamePage}`;
+            $.ajax({
+                url: queryUrl,
+                type: "GET"
+            }).then(results => {
+                //new page results 
+                console.log("RANDOM PAGE RESULTS", results)
+                //picks a random game
+                console.log(results.results[gameSlot])
+            })
         })
     });
 
