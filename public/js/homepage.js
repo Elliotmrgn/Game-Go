2830d0028fd6e04261a59951e573aa12416dd926
Elliot <elliotmrgn@gmail.com>
2020-05-27 20:21:13 -0400

    10k bug hack fix

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -20,27 +20,34 @@ $(document).ready(function () {
         }).then(result => {
             console.log(result)
             //picks a random game 1 - max
-            const randGame = Math.ceil(Math.random() * result.count)
+            let randGame = 10001;
+            do {
+                randGame = Math.ceil(Math.random() * result.count)
+            } while (randGame > 10000);
+            console.log("randGame", randGame)
             //finds what page a game is on
             const gamePage = Math.ceil(randGame / 20);
+            console.log("gamePage", gamePage)
             //find what array slot its in
             let gameSlot = (randGame % 20) - 1;
+            console.log("gameSlot", gameSlot)
             //case for last slot ****should be a better solution than this
             if (gameSlot === -1) { gameSlot = 19; }
             //new ajax call for that page
             queryUrl += `&page=${gamePage}`;
+            console.log("queryUrl", queryUrl)
             $.ajax({
                 url: queryUrl,
                 type: "GET"
             }).then(results => {
                 //new page results 
                 console.log("RANDOM PAGE RESULTS", results)
+
                 $.ajax({
                     url: `https://api.rawg.io/api/games/${results.results[gameSlot].id}`,
                     type: "GET"
                 }).then(game => {
                     console.log("****GAME*****", game)
-                    
                     //title update
                     $(".title").text(game.name)
                     //screenshot update
@@ -52,11 +59,7 @@ $(document).ready(function () {
                     });
                     $(".platforms").text(allPlatforms)
                     $(".description").html(game.description)
-
                 })
-
-                
-                
             })
         })
     });
