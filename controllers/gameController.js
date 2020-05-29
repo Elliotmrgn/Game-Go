c7e23654fedb055fbf962ed4061fdca2d30f0ef8
Jason Vernot <jason.vernot@gmail.com>
2020-05-28 22:23:37 -0400

    added files for saved games page. added middleware for handlebars. route for saved-games is now working

--- controllers/gameController.js
+++ controllers/gameController.js
@@ -3,14 +3,15 @@ const express = require("express");
 const router = express.Router();
 
 // Import the model to use its database functions.
-const db = require("../models");
+const db = require('../models');
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/test", (req, res) => {
-  db.game.findAll({})(data => {
-    const hbsObject = { games: data };
-    console.log(hbsObject);
-    //res.render("index", hbsObject);
+router.get("/saved-games", (req, res) => {
+    db.Game.findAll()
+      .then(data => {
+      console.log('These are the games!!!!!', data)
+      res.render("savedGames", data);
+    });
   });
-});
 
+module.exports = router;
